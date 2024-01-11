47ebdf44022e51857113f3a77186bae2f1b54217
thoriqagfi <thoriq.aghfi60@gmail.com>
2024-01-11 22:37:42 +0700

    fix: fix eslint

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,52 +5,52 @@ import './globals.css';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-	title: 'Thariq Agfi Hermawan',
-	description:
-		'Website portfolio developed by Thariq Agfi Hermawan. Showcasing a diverse range of creative projects and blogs.',
-	applicationName: 'Thariq Agfi Hermawan',
-	authors: [
-		{
-			name: 'Thariq Agfi Hermawan',
-			url: 'http://thoriqagfi.com/',
-		},
-	],
-	keywords: [
-		'thariq',
-		'agfi',
-		'hermawan',
-		'blog',
-		'portfolio',
-		'website',
-		'developer',
-	],
-	openGraph: {
-		type: 'website',
-		locale: 'en_US',
-		url: 'https://thoriqagfi.com/',
-		siteName: 'Thariq Agfi Hermawan',
-		images: [
-			{
-				url: 'https://thoriqagfi.com/images/og-image.png',
-				width: 1200,
-				height: 630,
-				alt: 'Thariq Agfi Hermawan',
-			},
-		],
-	},
-	twitter: {
-		site: '@thorikun',
-	},
+  title: 'Thariq Agfi Hermawan',
+  description:
+    'Website portfolio developed by Thariq Agfi Hermawan. Showcasing a diverse range of creative projects and blogs.',
+  applicationName: 'Thariq Agfi Hermawan',
+  authors: [
+    {
+      name: 'Thariq Agfi Hermawan',
+      url: 'http://thoriqagfi.com/',
+    },
+  ],
+  keywords: [
+    'thariq',
+    'agfi',
+    'hermawan',
+    'blog',
+    'portfolio',
+    'website',
+    'developer',
+  ],
+  openGraph: {
+    type: 'website',
+    locale: 'en_US',
+    url: 'https://thoriqagfi.com/',
+    siteName: 'Thariq Agfi Hermawan',
+    images: [
+      {
+        url: 'https://thoriqagfi.com/images/og-image.png',
+        width: 1200,
+        height: 630,
+        alt: 'Thariq Agfi Hermawan',
+      },
+    ],
+  },
+  twitter: {
+    site: '@thorikun',
+  },
 };
 
 export default function RootLayout({
-	children,
+  children,
 }: {
-	children: React.ReactNode,
+  children: React.ReactNode;
 }) {
-	return (
-		<html lang="en">
-			<body className={inter.className}>{children}</body>
-		</html>
-	);
+  return (
+    <html lang='en'>
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
 }
--- src/app/navbar.tsx
+++ src/app/navbar.tsx
@@ -1,30 +1,30 @@
 import Link from 'next/link';
 
 export default function Navbar() {
-	return (
-		<div className="flex flex-col justify-center items-center">
-			<div className="flex flex-row justify-between items-center w-full max-w-3xl px-4 py-4">
-				<div className="flex flex-row justify-center items-center">
-					<Link
-						className="font-bold text-2xl text-gray-900 dark:text-gray-100"
-						href="/"
-					>
-						Thariq Agfi Hermawan
-					</Link>
-				</div>
-				<div className="flex flex-row justify-center items-center">
-					<Link
-						className="font-bold text-lg text-gray-900 dark:text-gray-100"
-						href="/blog"
-					></Link>
-					<Link
-						className="font-bold text-lg text-gray-900 dark:text-gray-100 ml-4"
-						href="/portfolio"
-					>
-						Portfolio
-					</Link>
-				</div>
-			</div>
-		</div>
-	);
+  return (
+    <div className='flex flex-col justify-center items-center'>
+      <div className='flex flex-row justify-between items-center w-full max-w-3xl px-4 py-4'>
+        <div className='flex flex-row justify-center items-center'>
+          <Link
+            className='font-bold text-2xl text-gray-900 dark:text-gray-100'
+            href='/'
+          >
+            Thariq Agfi Hermawan
+          </Link>
+        </div>
+        <div className='flex flex-row justify-center items-center'>
+          <Link
+            className='font-bold text-lg text-gray-900 dark:text-gray-100'
+            href='/blog'
+          ></Link>
+          <Link
+            className='font-bold text-lg text-gray-900 dark:text-gray-100 ml-4'
+            href='/portfolio'
+          >
+            Portfolio
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 }
